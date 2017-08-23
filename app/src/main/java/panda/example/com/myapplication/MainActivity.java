12a6b4fb44f0c5a27263b749722ad9bc1822c5f8
XXASUS <306992765@qq.com>
2017-08-23 16:53:24 +0800

    刘鹏辉第一次添加Activity

--- app/src/main/java/panda/example/com/myapplication/MainActivity.java
+++ app/src/main/java/panda/example/com/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //李鑫
+        //刘鹏辉
     }
 }
