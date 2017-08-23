4c16ea03db1ba6f5d7389445635d9600b1481526
1296195902 <1296195902@qq.com>
2017-08-23 17:13:17 +0800

    111

--- app/src/main/java/panda/example/com/myapplication/MainActivity.java
+++ app/src/main/java/panda/example/com/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package panda.example.com.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,5 +21,7 @@ public class MainActivity extends AppCompatActivity {
         //方清正
         //方清正
         Log.e("", "onCreate:aaaaaaaaaaaaaaaaaaaaaaaaaaaaa ");
+        Intent it=new Intent(MainActivity.this,MainActivity.class);
+        startActivity(it);
     }
 }
