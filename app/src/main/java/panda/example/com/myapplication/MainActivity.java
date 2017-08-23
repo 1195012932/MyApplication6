2c2177bced23727736232648bcd79f85b9bed991
1296195902 <1296195902@qq.com>
2017-08-23 17:20:57 +0800

    111

--- app/src/main/java/panda/example/com/myapplication/MainActivity.java
+++ app/src/main/java/panda/example/com/myapplication/MainActivity.java
@@ -1,17 +1,24 @@
 package panda.example.com.myapplication;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button but;
+    private RelativeLayout activity_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
         //李鑫
         //刘鹏辉
         //黄振通
@@ -20,13 +27,30 @@ public class MainActivity extends AppCompatActivity {
         //方清正
         //方清正
         System.out.println("hahahahhaahhahahahhahaahahha");
-        Log.e("TGP","妈的，李老板，有哒哒哒哒哒冒蓝火的加特林吗？");
+        Log.e("TGP", "妈的，李老板，有哒哒哒哒哒冒蓝火的加特林吗？");
 
         Log.e("", "onCreate:aaaaaaaaaaaaaaaaaaaaaaaaaaaaa ");
-        Intent it=new Intent(MainActivity.this,MainActivity.class);
+        Intent it = new Intent(MainActivity.this, MainActivity.class);
         startActivity(it);
-        Intent its=new Intent(MainActivity.this,MainActivity.class);
+        Intent its = new Intent(MainActivity.this, MainActivity.class);
         startActivity(it);
         Toast.makeText(this, "jajfid", Toast.LENGTH_SHORT).show();
+    }
+
+    private void initView() {
+        but = (Button) findViewById(R.id.but);
+        activity_main = (RelativeLayout) findViewById(R.id.activity_main);
+
+        but.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.but:
+                Intent its = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(its);
+                break;
+        }
     }
 }
