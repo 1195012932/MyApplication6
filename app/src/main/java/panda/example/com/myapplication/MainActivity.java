afb49394bcdd208a8def11b4001b5feca40969f5
XXASUS <306992765@qq.com>
2017-08-23 17:18:31 +0800

    刘鹏辉第一次添加Activity

--- app/src/main/java/panda/example/com/myapplication/MainActivity.java
+++ app/src/main/java/panda/example/com/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +17,16 @@ public class MainActivity extends AppCompatActivity {
         //黄振通
         //第二次
         //第一次会有点疼
+        //方清正
+        //方清正
         System.out.println("hahahahhaahhahahahhahaahahha");
         Log.e("TGP","妈的，李老板，有哒哒哒哒哒冒蓝火的加特林吗？");
-        //方清正
-        //方清正
+
         Log.e("", "onCreate:aaaaaaaaaaaaaaaaaaaaaaaaaaaaa ");
         Intent it=new Intent(MainActivity.this,MainActivity.class);
         startActivity(it);
         Intent its=new Intent(MainActivity.this,MainActivity.class);
         startActivity(it);
+        Toast.makeText(this, "jajfid", Toast.LENGTH_SHORT).show();
     }
 }
