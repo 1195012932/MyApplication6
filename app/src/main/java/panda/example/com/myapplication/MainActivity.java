a3a50ef973dc3afeed7b019d52e6d809f92382f1
1195012932 <1195012932@qq.com>
2017-08-23 19:09:29 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/panda/example/com/myapplication/MainActivity.java

--- app/src/main/java/panda/example/com/myapplication/MainActivity.java
+++ app/src/main/java/panda/example/com/myapplication/MainActivity.java
@@ -3,11 +3,9 @@ package panda.example.com.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -19,22 +17,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-        //李鑫
-        //刘鹏辉
-        //黄振通
-        //第二次
-        //第一次会有点疼
-        //方清正
-        //方清正
-        System.out.println("hahahahhaahhahahahhahaahahha");
-        Log.e("TGP", "妈的，李老板，有哒哒哒哒哒冒蓝火的加特林吗？");
-
-        Log.e("", "onCreate:aaaaaaaaaaaaaaaaaaaaaaaaaaaaa ");
-        Intent it = new Intent(MainActivity.this, MainActivity.class);
-        startActivity(it);
-        Intent its = new Intent(MainActivity.this, MainActivity.class);
-        startActivity(it);
-        Toast.makeText(this, "jajfid", Toast.LENGTH_SHORT).show();
 
     }
 
